567f28704789761fa2e3137d2786052a91a33c89
Yash Pandya <pandya.y@husky.neu.edu>
2019-09-22 04:13:51 -0400

    create assignment

---
+++ Assignment1_Yash_Pandya/src/Business/Product.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author hp
+ */
+public class Product {
+    
+    private String modelName;
+    private String serialNumber;
+    private String color;
+    private String speed;
+    private String mileage;
+    private String manufactureCountry;
+    private String manufactureYear;
+    private String numberOfDoors;
+    private String numberOfGears;
+    private String numberOfAirBags;
+    private String interiorType;
+    private String engineSize;
+    private String GasTank;
+    private String roofHeight;
+    private String roadClearance;
+    private String price;
+    private String image;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(String mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getManufactureCountry() {
+        return manufactureCountry;
+    }
+
+    public void setManufactureCountry(String manufactureCountry) {
+        this.manufactureCountry = manufactureCountry;
+    }
+
+    public String getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(String manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public String getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(String numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public String getNumberOfGears() {
+        return numberOfGears;
+    }
+
+    public void setNumberOfGears(String numberOfGears) {
+        this.numberOfGears = numberOfGears;
+    }
+
+    public String getNumberOfAirBags() {
+        return numberOfAirBags;
+    }
+
+    public void setNumberOfAirBags(String numberOfAirBags) {
+        this.numberOfAirBags = numberOfAirBags;
+    }
+
+    public String getInteriorType() {
+        return interiorType;
+    }
+
+    public void setInteriorType(String interiorType) {
+        this.interiorType = interiorType;
+    }
+
+    public String getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(String engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public String getGasTank() {
+        return GasTank;
+    }
+
+    public void setGasTank(String GasTank) {
+        this.GasTank = GasTank;
+    }
+
+    public String getRoofHeight() {
+        return roofHeight;
+    }
+
+    public void setRoofHeight(String roofHeight) {
+        this.roofHeight = roofHeight;
+    }
+
+    public String getRoadClearance() {
+        return roadClearance;
+    }
+
+    public void setRoadClearance(String roadClearance) {
+        this.roadClearance = roadClearance;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    
+    
+}
